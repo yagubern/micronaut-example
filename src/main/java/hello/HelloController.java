c2332753044e2b442a0d13184e87b4e807c0fbf8
Yaroslav Gubernatorov <ygubernatoro@avaya.com>
2021-11-02 15:13:40 +0300

    Added simple micronaut app

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,12 @@
+package hello;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
