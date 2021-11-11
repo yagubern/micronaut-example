a877a1729d1cde758918d944bf5a9521380b51a6
Yaroslav Gubernatorov <ygubernatoro@avaya.com>
2021-11-11 15:07:30 +0300

    Update to v1beta1 and add triggers
    Moved pipeline and task resource from v1alpha1 to v1beta1
    Added GitHub event litener

---
+++ tekton/triggers/curl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -v \
+-H 'X-GitHub-Event: pull_request' \
+-H 'X-Hub-Signature: sha1=5e5c6e4674a394f7f0f0352f2b02547c7793f627' \
+-H 'Content-Type: application/json' \
+-d '{"action": "opened", "pull_request":{"head":{"sha": "47cebedcad9ec95cbb4b405bfcc63f6aac0e3fc7"}},"repository":{"clone_url": "https://github.com/yagubern/micronaut-example.git"}}' \
+http://localhost:8080
+
+#tkn eventlistener logs github-listener
