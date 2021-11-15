8966541873133b09c676e3781e3aea47105808c5
Yaroslav Gubernatorov <ygubernatoro@avaya.com>
2021-11-15 10:05:08 +0300

    Added ingress for webhook

--- tekton/triggers/curl.sh
+++ tekton/triggers/curl.sh
@@ -4,6 +4,8 @@ curl -v \
 -H 'X-Hub-Signature: sha1=5e5c6e4674a394f7f0f0352f2b02547c7793f627' \
 -H 'Content-Type: application/json' \
 -d '{"action": "opened", "pull_request":{"head":{"sha": "47cebedcad9ec95cbb4b405bfcc63f6aac0e3fc7"}},"repository":{"clone_url": "https://github.com/yagubern/micronaut-example.git"}}' \
-http://localhost:8080
+http://34.145.107.102
+
+#http://localhost:8080
 
 #tkn eventlistener logs github-listener
